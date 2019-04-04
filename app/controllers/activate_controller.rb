7abcf220fc648156170e099ac2a6de2d16c0086b
Yixu Chen <cyx709671676@gmail.com>
2019-04-04 16:27:56 -0500

    add company when approved

--- app/controllers/activate_controller.rb
+++ app/controllers/activate_controller.rb
@@ -1,19 +1,21 @@
 class ActivateController < ApplicationController
-    def activate
-      if admin?
-        @user = User.find_by(login_id: params[:login_id])
-        if !@user.approved
-          @user.create_activation_digest
-          UserMailer.account_activation(@user).deliver_now
-          flash[:success] = "Successfully send email"
-          redirect_to users_url
-        else
-          flash[:warning] = "User Already Activated!"
-          redirect_to users_url
-        end
+  before_action :logged_in_user
+  def activate
+    if admin?
+      @user = User.find_by(login_id: params[:login_id])
+      if !@user.approved
+        @user.create_activation_digest
+        @user.create_company
+        UserMailer.account_activation(@user).deliver_now
+        flash[:success] = "Successfully send email"
+        redirect_to users_url
       else
-        flash[:danger] = "You do not have permission."
-        redirect_to root_url
+        flash[:warning] = "User Already Activated!"
+        redirect_to users_url
       end
+    else
+      flash[:danger] = "You do not have permission."
+      redirect_to root_url
     end
+  end
 end
