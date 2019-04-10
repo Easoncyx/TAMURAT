e40878a33cdc3b6dc48fa230fe21d81e73d96f01
boquan <taoboquan1@gmail.com>
2019-04-10 21:52:36 +0000

    heroku email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://rocky-sea-36177.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  host = 'limitless-eyrie-57825.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
