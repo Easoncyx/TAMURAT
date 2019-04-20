160aa99af1470b00a9ef14f52746a878881e7fbf
Yixu Chen <cyx709671676@gmail.com>
2019-04-20 13:25:18 -0500

    calculate score and new edit scenarios view

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -2,10 +2,21 @@ class Scenario < ApplicationRecord
   has_many :privileges, class_name: "Privilege", foreign_key: "scenario_id", dependent: :destroy
   has_many :users, through: :privileges
   has_many :scenario_weights, class_name: "ScenarioWeight", foreign_key: "scenario_id", dependent: :destroy
-  has_many :companys, through: :scenario_weights 
+  has_many :companys, through: :scenario_weights
 
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 250 }
 
+  def cal_scenario_score()
+    total_score = 0
+    self.scenario_weights.each do |wei|
+      company = Company.find_by_id(wei.company_id)
+      if company.validated
+        total_score += company.score * wei.weight
+      end
+    end
+    self.score = total_score
+    self.save
+  end
 end
