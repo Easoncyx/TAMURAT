0f5ea2d72eba624613f674f04b3b34e57c3df7b6
yufeiyan1220 <yufeiyan1220@live.com>
2019-04-20 23:28:19 +0000

    add gravater icon of outputs company.

--- app/helpers/outputs_helper.rb
+++ app/helpers/outputs_helper.rb
@@ -20,11 +20,12 @@ module OutputsHelper
     end
     
     name = User.find_by_id(obj.user_id).name
+    img = gravatar(User.find_by_id(obj.user_id), size: 20)
     result = {
       "id"          => obj.id, 
       "text"        => name,
       "parent"      => parent,
-      "icon"        => "string",
+      "icon"        => img,
       "state"       => {
         "opened"    => true,  
       },
@@ -33,4 +34,12 @@ module OutputsHelper
     }
     return result
   end
+  
+  def gravatar(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    return gravatar_url
+  end
+  
 end
