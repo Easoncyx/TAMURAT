b863339f866c57ea1e4c28e28ce0c6c1148de14e
shibowang <ec2-user@ip-172-31-35-169.us-east-2.compute.internal>
2019-04-23 18:46:22 +0000

    Add Formulae

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,31 @@
 class StaticPagesController < ApplicationController
+  
+ 
+  
+  
+  
+  
   def home
     @user = current_user
     @scenario = current_user.scenarios.build if logged_in?
   end
 
   def help
+    
+    @category = Category.order(:id)
+    #byebug
+    @result = {}
+    @subcategory = Subcategory.order(:id)
+    @category_weight_sum=0
+    @category.each do |ctgr|
+      @category_weight_sum += ctgr.weight
+      subcategory = Subcategory.where("category_id = ?", ctgr.id)
+      @result[ctgr] = {}
+      subcategory.each do |subcate|
+        @result[ctgr][subcate] = Question.where("subcategory_id = ?", subcate.id).order(:id)
+      end
+    end
+    
   end
 
   def about
@@ -12,4 +33,19 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+  
+  
+  private
+
+    
+    def correct_user
+      if !admin? && !decision_maker?
+
+        flash[:danger] = "Please log in as correct user."
+        redirect_to root_url and return
+      end
+    end
+  
+  
+  
 end
