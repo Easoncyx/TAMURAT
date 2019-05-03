57e30c5c21873344bb0911fa2a11c3b42ecf3c70
Boquan Tao <taoboquan1@gmail.com>
2019-05-03 16:28:30 +0000

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://serene-lowlands-61019.herokuapp.com/'
+  host = 'fast-shore-38378.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
